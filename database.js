4566883e9806b47780381d9e529469d0494fbf86
Nicolas Sacandy <nicsacandy@gmail.com>
2020-04-04 11:22:52 -0400

    Landing page updated
    Username still needed, need to start configuring server side.

--- database.js
+++ database.js
@@ -1,6 +1,25 @@
-let customer;
+let user;
+let review;
+let business;
 
-function assignCustomer(){
-this.customer=document.getElementById('username').innerHTML;
-document.getElementById
+function assignCustomer(userName) {
+    this.user = userName;
+    if (this.user === undefined) {
+        this.user = "undefined";
+    }
 }
+
+function assignBusiness(businessName) {
+    this.business = businessName;
+    if (this.business === undefined) {
+        this.business = "undefined";
+    }
+}
+
+function createReview(userReview) {
+    this.review = userReview;
+    if (this.userReview === undefined) {
+        this.userReview = "undefined";
+    }
+}
+
